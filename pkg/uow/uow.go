ebbfa3227dba0b92c3b81f468cc98058e04e7508
aricamargo_meli <dario.camargo@mercadolivre.com>
2024-09-17 20:32:15 -0300

    uow interface

--- pkg/uow/uow.go
+++ pkg/uow/uow.go
@@ -1,9 +1,17 @@
 package uow
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type RepositoryFactory func(tx *sql.Tx) interface{}
 
 type UowInterface interface {
 	Register(name string, fc RepositoryFactory)
+	GetRepository(ctx context.Context, name string) (interface{}, error)
+	Do(ctx context.Context, fn func(uow *UowInterface) error) error
+	CommitOrRollback() error
+	Rollback() error
+	UnRegister(name string)
 }
